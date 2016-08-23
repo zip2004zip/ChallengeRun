1ec00c558c1102bfba2f828d323e26d9b7705675
agronin.al <agronin.al@gmail.com>
2016-08-23 20:16:42 +0300

    new project

---
+++ app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
@@ -0,0 +1,13 @@
+package by.mix.challengerun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChallengeRunActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_run);
+    }
+}
