560d549470f1c840847c8de2570b9f654930d493
agronin.al <agronin.al@gmail.com>
2016-08-23 20:59:33 +0300

    layout ch run

--- app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
+++ app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
@@ -11,3 +11,7 @@ public class ChallengeRunActivity extends AppCompatActivity {
         setContentView(R.layout.activity_challenge_run);
     }
 }
+
+
+
+//http://wiki.gis-lab.info/w/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_%D0%B8_%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE_%D0%B0%D0%B7%D0%B8%D0%BC%D1%83%D1%82%D0%B0_%D0%BC%D0%B5%D0%B6%D0%B4%D1%83_%D0%B4%D0%B2%D1%83%D0%BC%D1%8F_%D1%82%D0%BE%D1%87%D0%BA%D0%B0%D0%BC%D0%B8_%D0%BD%D0%B0_%D1%81%D1%84%D0%B5%D1%80%D0%B5
