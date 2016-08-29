a44dcb95bf152f1aff0d033b76b05d2e40c83d40
zip2004zip <zip2004zip@mail.ru>
2016-08-30 01:17:02 +0300

    YourSelf Map

--- app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
+++ app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
@@ -1,17 +1,40 @@
 package by.mix.challengerun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ChallengeRunActivity extends AppCompatActivity {
+public class ChallengeRunActivity extends AppCompatActivity implements View.OnClickListener {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_run);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_challenge_yourself:
+                intent = new Intent(this, ChallengeYourselfActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_challenge_friend:
+                intent = new Intent(this, ChallengeFriendActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_challenge_team:
+                intent = new Intent(this, ChallengeTeamActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
 
+// карты гугл API
+// https://developers.google.com/maps/documentation/android-api/?csw=1
 
-
-//http://wiki.gis-lab.info/w/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_%D0%B8_%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE_%D0%B0%D0%B7%D0%B8%D0%BC%D1%83%D1%82%D0%B0_%D0%BC%D0%B5%D0%B6%D0%B4%D1%83_%D0%B4%D0%B2%D1%83%D0%BC%D1%8F_%D1%82%D0%BE%D1%87%D0%BA%D0%B0%D0%BC%D0%B8_%D0%BD%D0%B0_%D1%81%D1%84%D0%B5%D1%80%D0%B5
+// формулы измерения расстояния координат
+// http://wiki.gis-lab.info/w/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_%D0%B8_%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE_%D0%B0%D0%B7%D0%B8%D0%BC%D1%83%D1%82%D0%B0_%D0%BC%D0%B5%D0%B6%D0%B4%D1%83_%D0%B4%D0%B2%D1%83%D0%BC%D1%8F_%D1%82%D0%BE%D1%87%D0%BA%D0%B0%D0%BC%D0%B8_%D0%BD%D0%B0_%D1%81%D1%84%D0%B5%D1%80%D0%B5
---
+++ app/src/main/java/by/mix/challengerun/ChallengeYourselfActivity.java
@@ -0,0 +1,87 @@
+package by.mix.challengerun;
+
+import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+//public class ChallengeYourselfActivity extends AppCompatActivity {
+public class ChallengeYourselfActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
+//    private boolean wasRunning; // хранение информации о том, работал ли секундомер перед вызовом метода onStop()
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_yourself);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            //  wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+/*    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }*/
+
+    // сохранение показаний перед уничтожением секундомера
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        //   savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    //Запустить секундомер при щелчке на кнопке Start.
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    //Остановить секундомер при щелчке на кнопке Stop.
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    //Обнулить секундомер при щелчке на кнопке Reset.
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    //Обновление показаний таймера
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.textView_RuntimeRecorder);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
