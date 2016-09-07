35b19c1648f8765c57b1be1de613b5dcbf50e232
zip2004zip <zip2004zip@mail.ru>
2016-09-08 01:43:56 +0300

    location GPS speed, altitude

--- app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
+++ app/src/main/java/by/mix/challengerun/ChallengeRunActivity.java
@@ -26,7 +26,7 @@ public class ChallengeRunActivity extends AppCompatActivity implements View.OnCl
                 startActivity(intent);
                 break;
             case R.id.button_challenge_team:
-                intent = new Intent(this, ChallengeTeamActivity.class);
+                intent = new Intent(this, LocationGPSActivity.class);
                 startActivity(intent);
                 break;
         }
@@ -35,6 +35,9 @@ public class ChallengeRunActivity extends AppCompatActivity implements View.OnCl
 
 // карты гугл API
 // https://developers.google.com/maps/documentation/android-api/?csw=1
+
+// http://www.enterra.ru/blog/gps-android/ Работа с GPS на Android
+// http://www.darkraha.com/rus/mobile/android/api/location.php методы класса Location на русском
 
 // формулы измерения расстояния координат
 // http://wiki.gis-lab.info/w/%D0%92%D1%8B%D1%87%D0%B8%D1%81%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D1%80%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D1%8F_%D0%B8_%D0%BD%D0%B0%D1%87%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B3%D0%BE_%D0%B0%D0%B7%D0%B8%D0%BC%D1%83%D1%82%D0%B0_%D0%BC%D0%B5%D0%B6%D0%B4%D1%83_%D0%B4%D0%B2%D1%83%D0%BC%D1%8F_%D1%82%D0%BE%D1%87%D0%BA%D0%B0%D0%BC%D0%B8_%D0%BD%D0%B0_%D1%81%D1%84%D0%B5%D1%80%D0%B5
--- app/src/main/java/by/mix/challengerun/LocationGPSActivity.java
+++ app/src/main/java/by/mix/challengerun/LocationGPSActivity.java
@@ -1,16 +1,24 @@
 package by.mix.challengerun;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LocationGPSActivity extends AppCompatActivity {
+
+    private TextView textViewLocGPS;
+    private TextView textViewLocSpeed;
+    private TextView textViewLocAltitude;
 
     private LocationManager locationManager;
     private LocationListener locationListener;
@@ -20,11 +28,24 @@ public class LocationGPSActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_gps);
 
+        textViewLocGPS = (TextView) findViewById(R.id.textView_locationGPS);
+        textViewLocSpeed = (TextView) findViewById(R.id.textView_LocationSpeed);
+        textViewLocAltitude = (TextView) findViewById(R.id.textView_LocationAltitude);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // отслеживает изменение местоположения
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                location.getLatitude();
+                location.getLongitude();
+                location.getSpeed();
+                location.getAltitude();
+                String txt = "Текущее местоположение: " + "\nLatitud = " + location.getLatitude() + "\nLongitud = " + location.getLongitude();
+                textViewLocGPS.setText(txt);
+                textViewLocSpeed.setText("Текущая скорость: " + "\n" + location.getSpeed() + " км/ч");
+                textViewLocAltitude.setText("Текущая высота: " + "\n" + location.getAltitude() + " м");
             }
 
             @Override
@@ -34,12 +55,15 @@ public class LocationGPSActivity extends AppCompatActivity {
 
             @Override
             public void onProviderEnabled(String s) {
-
+                Toast.makeText(getApplicationContext(), "Gps включен", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onProviderDisabled(String s) {
-
+                Toast.makeText(getApplicationContext(), "Gps выключен", Toast.LENGTH_LONG).show();
+                // вызываем настройки для включения GPS
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
             }
         };
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -58,8 +82,46 @@ public class LocationGPSActivity extends AppCompatActivity {
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
     }
+
+    @Override
+    protected void onResume() {
+        // включаем отслеживание при GPS
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+
+        // при использовании сетей типа GSM
+        // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        // выключаем отслеживание
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
+        super.onPause();
+    }
 }
 
 
-
 // https://youtu.be/QNb_3QKSmMk
+// http://www.darkraha.com/rus/mobile/android/api/location.php
